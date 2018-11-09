f32ad50767e4126d08956d739d68243b94b7d464
Marco Wang <37522841+MarcoXM@users.noreply.github.com>
2018-11-09 14:38:36 -0500

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -100,10 +100,13 @@ for i in range(d_catg):
 
 ##########################################
 #Plotting numeric hist
+numeric = list(dataset.describe().columns)
+'''
 numeric = ['admission_type_id', 'discharge_disposition_id', 'admission_source_id',
        'time_in_hospital', 'num_lab_procedures', 'num_procedures',
        'num_medications', 'number_outpatient', 'number_emergency',
        'number_inpatient', 'number_diagnoses']
+'''
 
 # Hist graphes for numeric features:
 for i in numeric:
